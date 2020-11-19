2a3a5272fb4227a3bc2ffb648fafc7060cb187b8
giann-bnd07 <giann_marasigan@dlsu.edu.ph>
2020-11-19 19:53:15 +0800

    current progress for signup; mergedfrom developing

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,123 @@
+const bcrypt = require('bcrypt');
+const mongoose = require('mongoose');
+const { validationResult } = require('express-validator');
+const session = require('express-session');
+const fs = require('fs');
+const db = require('../models/db');
+const User = require('../models/patientModel');
+const Doctor = require('../models/doctorModel');
+const helper = require('../helpers/helper');
+const saltRounds = 10;
+
 const userController = {
-	login: function(req,res){
+	getLogin: function(req,res){
 		res.render('login')
 	},
     
-	register: function(req,res){
-		res.render('register')
+	getRegister: function(req,res){
+		res.render('register', {
+			title: 'Register | Med-Aid',
+			register_active: true,
+		});
+	},
+
+	postRegister: function(req, res) {
+		var errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            errors = errors.errors;
+
+            var details = {};
+            for (let i = 0; i < errors.length; i++)
+                details[errors[i].param + 'Error'] = errors[i].msg;
+
+            res.render('register', {
+                inputs: req.body,
+                details: details,
+                title: 'Register | Med-Aid',
+                register_active: true,
+            });
+        } else {
+			//sanitize use inputs
+			const fname = helper.sanitize(req.body.firstname);
+			const lname = helper.sanitize(req.body.lastname);
+			const email = helper.sanitize(req.body.email);
+			
+			bcrypt.hash(password, saltRounds, (err, hash) => {
+				if(!req.files['picture']) {
+					var USER = new User({
+						usertype: 'patient',
+						firstname: fname,
+						lastname: lname,
+						email: email,
+						password: hash
+					});
+
+					db.findOne(User, USER, function(flag){
+						if (flag) {
+							res.redirect('/');
+						}
+					});
+				}
+
+				else {
+					var USER = new User({
+						usertype: 'patient',
+						firstname: fname,
+						lastname: lname,
+						email: email,
+						password: hash
+					});
+
+					var picName = USER.firstname;
+					var picFileName = helper.renameAvatar(req, picName);
+					USER.profpic = picFileName;
+
+					db.findOne(User, USER, function(flag){
+						if (flag) {
+							res.redirect('/');
+						}
+					});
+				}
+			});
+        
+		}
+	},
+/*
+	postRegisterDoc: function (req, res) {
+		console.log(req.body);
+		var errors = validationResult(req);
+
+		if (!errors.isEmpty()) {
+			errrors = errors.errors;
+
+			var details = {};
+
+			for (let i = 0; i < errors.length; i++) {
+                // remove array indices for wildcard checks
+                details[`${errors[i].param.replace(/\[\d\]/g, '')}Error`] =
+                    errors[i].msg;
+			}
+			res.render('register', {
+                inputs: req.body,
+                details: details,
+                title: 'Register | Med-Aid',
+                register_active: true,
+            });
+		} else {
+			const o = {};
+			for (const field in req.body) {
+				if(req.body.hasOwnProperty(field)) {
+					o[field] = helper.sanitize(req.body[field]);
+				}
+			}
+
+			var doctor = {
+				firstname: o.firstname
+			}
+		}
 	}
+*/
 }
 
-module.exports = userController
+module.exports = userController;
---
+++ helpers/helper.js
@@ -0,0 +1,102 @@
+const sanitize = require('mongo-sanitize');
+const mongoose = require('mongoose');
+const moment = require('moment');
+const dateformat = require('dateformat');
+const fs = require('fs');
+const db = require('../models/db');
+
+const helper = {
+    sanitize: function (query) {
+        return sanitize(query);
+    },
+
+    renameAvatar: function (req, newName) {
+        var origName = req.files['avatar'][0].originalname;
+        var extension = origName.substring(origName.lastIndexOf('.'));
+        const newURL =
+            req.files['avatar'][0].destination + '/' + newName + extension;
+
+        fs.renameSync(req.files['avatar'][0].path, newURL);
+        return newName + extension;
+    },
+
+    renameResume: function (req, newName) {
+        var origName = req.files['resume'][0].originalname;
+        var extension = origName.substring(origName.lastIndexOf('.'));
+        const newURL =
+            req.files['resume'][0].destination + '/' + newName + extension;
+
+        fs.renameSync(req.files['resume'][0].path, newURL);
+        return newName + extension;
+    },
+
+    formatDate: function (date) {
+        var diff_seconds = Math.round((+new Date() - date) / 1000);
+
+        var minute = 60,
+            hour = minute * 60,
+            day = hour * 24,
+            week = day * 7;
+
+        var time;
+
+        if (diff_seconds < 30) {
+            time = 'just now';
+        } else if (diff_seconds < minute) {
+            time = diff_seconds + ' seconds ago';
+        } else if (diff_seconds < 2 * minute) {
+            time = 'a minute ago';
+        } else if (diff_seconds < hour) {
+            time = Math.floor(diff_seconds / minute) + ' minutes ago';
+        } else if (Math.floor(diff_seconds / hour) == 1) {
+            time = '1 hour ago';
+        } else if (diff_seconds < day) {
+            time = Math.floor(diff_seconds / hour) + ' hours ago';
+        } else if (diff_seconds == day) {
+            time = 'yesterday';
+        } else if (diff_seconds < day * 7){
+            var temp = Math.floor(diff_seconds / day);
+
+            if (temp == 1){
+                time = '1 day ago'
+            } else{
+                time = Math.floor(diff_seconds / day) + ' days ago';
+            }
+        } else if (diff_seconds == week) {
+            time = '1 week ago'; 
+        } else if (diff_seconds == week * 2){
+        	time = '2 weeks ago'; 
+		} else if (diff_seconds == week * 3){
+        	time = '3 weeks ago'; 
+		} else if (diff_seconds == week * 4){
+        	time = '1 month ago'; 
+		} else if ((diff_seconds > week) && (diff_seconds < week * 2)) {
+			time = 'more than a week ago' 
+        } else if ((diff_seconds > week) && (diff_seconds < week * 4)) {
+			time = 'more than ' + Math.floor(diff_seconds / week) + ' weeks ago' 
+        } else {
+            time = 'on ' + dateformat(date, 'mmm dd, yyyy');
+        }
+
+        return time;
+    },
+
+    parseDate: function (s) {
+        if (!moment(s, 'YYYY-MM-DD', true).isValid()) {
+            return null;
+        }
+
+        if (s == null || s === undefined) {
+            return null;
+        }
+
+        var b = s.split(/\D/);
+        let date = new Date(b[0], --b[1], b[2]);
+
+        date.setHours(8, 0, 0, 0);
+
+        return date;
+    }
+};
+
+module.exports = helper;

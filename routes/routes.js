2dd41fed4a9f8ed43d6b2d1f87e45746820d2cb9
reqinamarie <reg.masilang@gmail.com>
2020-11-19 17:38:41 +0800

    added addClinic route

--- routes/routes.js
+++ routes/routes.js
@@ -14,6 +14,7 @@ module.exports = app
 app.get('/adminClinics', adminController.clinics)
 app.get('/adminDoctors', adminController.doctors)
 app.get('/adminPending', adminController.pending)
+app.post('/addClinic', adminController.addClinic)
 
 app.get('/upcomingAppointments', appointmentController.upcomingAppointments)
 app.get('/pendingAppointments', appointmentController.pendingAppointments)

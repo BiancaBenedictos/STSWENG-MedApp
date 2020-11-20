e5a9a7575ecde662f62a30510d22fcfc16917a92
michaeladizon <michaela_dizon@dlsu.edu.ph>
2020-11-20 20:46:04 +0800

    fixed clinic dropdown

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,15 @@
+const db = require('../models/db.js');
+const Clinic = require('../models/clinicModel.js');
+
 const userController = {
 	login: function(req,res){
 		res.render('login')
 	},
     
 	register: function(req,res){
-		res.render('register')
+		db.findMany(Clinic, {}, null, function(clinics) {
+			res.render('register', {clinics: clinics})
+		})
 	}
 }
 

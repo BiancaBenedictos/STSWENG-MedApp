df883f8a767465863f11c95c1e7db712c32868ce
michaeladizon <michaela_dizon@dlsu.edu.ph>
2020-11-20 23:25:39 +0800

    fixed add clinic dropdown

--- views/scripts/register.js
+++ views/scripts/register.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-    console.log("hello")
     document.getElementById('doctorCheck').onclick = function() {
         var checkBox = document.getElementById("doctorCheck")
         var text = document.getElementById("doctorFields")
@@ -11,23 +10,6 @@ $(document).ready(function() {
         }
     }
 
-    document.getElementById('addClinic').onclick = function() {
-        var values = ["Clinic 1", "Clinic 2", "Clinic 3"]
-
-        var select = document.createElement("select")
-        select.id = "clinic"
-        select.className = "form-control"
-
-        for (const val of values) {
-            var option = document.createElement("option")
-            option.value = val
-            option.text = val.charAt(0).toUpperCase() + val.slice(1)
-            select.appendChild(option)
-        }
-
-        document.getElementById("addRowsHere").appendChild(select)
-    }
-    
     document.getElementById('registerClinic').onclick = function() {
         var input = document.createElement("input")
         input.id = "clinic"

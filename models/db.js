1b3f5a2b91f49e7ffaf90aff23dc6e8275d70f61
michaeladizon <michaela_dizon@dlsu.edu.ph>
2020-11-21 11:01:11 +0800

    Update db.js

--- models/db.js
+++ models/db.js
@@ -65,7 +65,7 @@ const database = {
 
     updateMany: function(model, filter, update) {
         model.updateMany(filter, update, function(error, result) {
-            if(error) return callback(false);
+            // if(error) return callback(false);
             //console.log('Documents modified: ' + result.nModified);
             // return callback(true);
         });

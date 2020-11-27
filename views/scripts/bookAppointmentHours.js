8723546515613cda85c803e516c7efe6db9d299b
reqinamarie <reg.masilang@gmail.com>
2020-11-27 16:02:30 +0800

    implemented posting of appointment details

--- views/scripts/bookAppointmentHours.js
+++ views/scripts/bookAppointmentHours.js
@@ -10,18 +10,24 @@ function getSlots(day, doctor, clinic) {
     $.get('/getSlots', {day: day, doctorID: doctor, clinicID: clinic}, function(results){
         for (i=0; i<results.length; i++)
             d.append('<button class="time" data-toggle="modal" data-target="#confirm"' +
-                        'onclick="updateBookTime(\'' + results[i] + '\')">' + results[i] + '</button>')
+                        'onclick="updateBookTime(\'' + results[i].H12 + '\', \'' + results[i].H24 + '\')">' + results[i].H12 + '</button>')
         if (results.length == 0) {
             d.append('<p>No available appointments.</p>')
         }
     })
 }
 
-function updateBookTime(time) {
+function updateBookTime(time12, time24) {
     month = $("span.month").attr('id');
     date = $('button.active').text();
-    time = time;
-    $("#book-time").text(month + " " + date + ", " + time);
+    time = time24;
+    $("#book-time").text(month + " " + date + ", " + time12);
+}
+
+function bookAppointment() {
+    $.post('/requestAppointment', {month: month, date: date, time: time, doctor: $("h1.doctor").attr('id')}, function(res) {
+        console.log(res);
+    })
 }
 
 $(document).ready(function(){

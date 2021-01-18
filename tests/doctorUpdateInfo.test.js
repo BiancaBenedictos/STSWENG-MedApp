b0a97cfb50731db3467980db2354e0fa546d2614
reqinamarie <reg.masilang@gmail.com>
2021-01-18 21:07:39 +0800

    changed db to local; implemented create and drop collections

--- tests/doctorUpdateInfo.test.js
+++ tests/doctorUpdateInfo.test.js
@@ -1,53 +1,106 @@
 jest.useFakeTimers()
-const httpMocks = require('node-mocks-http')
-//const app = require('../index') // Link to your server file
+
 const express = require('express');
-
-
-const app = require('../testApp') // Link to your server file
-
+const app = require('../testApp') 
 const supertest = require('supertest')
 const request = supertest.agent(app)
 
-jest.setTimeout(100000)
 const Doctor = require('../models/doctorModel')
+const Clinic = require('../models/clinicModel')
 
 const mongoose = require('mongoose');
 const { expectation } = require('sinon');
-beforeAll(async () => {
-    // const url = `mongodb://127.0.0.1/testDB`
-    const url = 'mongodb+srv://admin:123@meddb.bbgb8.mongodb.net/local_library'
+jest.setTimeout(10000)
+
+const bcrypt = require('bcrypt');
+const helper = require('../helpers/helper');
+const saltRounds = 10;
+
+var doctorID, clinicIDs
+beforeAll(async (done) => {
+    const url = 'mongodb://127.0.0.1/testDB'
     await mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-  })
+    await mongoose.connection.db.dropCollection('doctors');
+    await mongoose.connection.db.dropCollection('clinics');
 
+    /* PREPARE DB */
+    var clinics = [{
+        clinicDoctors: [],
+        clinicName: 'Test Clinic',
+        clinicAddress: {
+        street: 'test',
+        city: 'clinic',
+        state: 'address'
+        }
+    }, {
+        clinicDoctors: [],
+        clinicName: 'Test 2',
+        clinicAddress: {
+        street: 'test',
+        city: '2',
+        state: 'address'
+        },
+        __v: 0
+    }]
 
-/*
-beforeEach(async function(done){
-    await request(app)
-      .post('/')
-      .send({email: 'test@dr.com', password: 'test'})
-      .end(function(err, response){
-          console.log(response)
-        expect(response.statusCode).to.equal(200);
-        done();
-      });
-  });
-  */
+    try {
+        await Clinic.insertMany(clinics)
+        
+        await Clinic.find({}, '_id', async function(err, res) {
+            clinicIDs = res.map(s => s._id);
+
+            bcrypt.hash(`test`, saltRounds, async (err, hash) => {
+                await Doctor.create({
+                    clinics: clinicIDs,
+                    profpic:"images/test.png",
+                    bookedAppointments:[],
+                    availability:[],
+                    email:"test@dr.com",
+                    password: hash,
+                    firstname:"test",
+                    lastname:"update",
+                    profession:"Pediatrician",
+                    status:"verified",
+                    credentials:"doctor.pdf",
+                })
+                
+                Doctor.findOne({}, '_id', async function(err, result) {
+                    doctorID = result._id
+                    await Clinic.updateMany({}, result._id)
+                    done()
+                })
+            })
+        }).lean()
+    } catch (ex) {
+        console.log(ex)
+    }
+    
+})
+
+afterAll(async () => {
+//    await mongoose.disconnect()
+    await mongoose.connection.db.dropCollection('sessions');
+ //   await mongoose.connection.db.dropDatabase();
+})
+
 
 describe('Update doctor info', () => {
     it('login', loginUser())
     it('test', async done => {
         try {
-
-            var rq = {
-                info: {
-                    firstname: 'test',
-                    lastname: 'update',
-                    email: 'test@dr.com',
-                    profession: 'Pediatrician',
-                    clinics: [ '5fb59a0731422020ec5fb2e0', '5fb77b1c86c5004b18e8223b' ] 
+            var rq
+            await Clinic.findOne({}, "_id", async function(err, res) {
+                rq = {
+                    info: {
+                        firstname: 'test',
+                        lastname: 'update',
+                        email: 'test@dr.com',
+                        profession: 'Pediatrician',
+                        clinics: [res._id.toString()]
+                    }
                 }
-            }
+            })
+
             const editProf = await request.        
                 post('/editProfile').type('form').send(rq)
 
@@ -55,14 +108,11 @@ describe('Update doctor info', () => {
                 expect(rq.info).toMatchObject(result)
                 done()
             }).lean()
+            
+            
 
-
-    
-    
         } catch (e) {
-            console.log("------------ERROR-----------")
             console.log(e)
-            console.log("------------ERROR-----------")
         }
     
     })
@@ -73,19 +123,18 @@ describe('Update doctor info', () => {
 function loginUser() {
     return async done => {
         try {
-    
             const login = await request.        
                 post('/').type('form').send({
                     email: 'test@dr.com',
                     password: 'test'
                 })
             
-            done()    
-    
+            done()        
         } catch (e) {
             console.log("------------ERROR-----------")
             console.log(e)
             console.log("------------ERROR-----------")
         }
     }
+    
 };
